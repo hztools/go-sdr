c19dbdab1cd68c0f4c4715206c538e0411e7ac18
Paul R. Tagliamonte <paultag@gmail.com>
2021-03-08 20:46:29 -0500

    Fix range after read
    Tracking down a bug in a depending lib, I've found this class of bug to
    be both common and annoying. If reading from a nested reader, always
    slice the resulting Samples.

--- stream/rotate.go
+++ stream/rotate.go
@@ -52,7 +52,7 @@ func (mr *multiplyReader) Read(s sdr.Samples) (int, error) {
 
 	// TODO(paultag): Fix this to be safe when the above format checks
 	// grow.
-	sC64 := s.(sdr.SamplesC64)
+	sC64 := s.(sdr.SamplesC64)[:i]
 	sC64.Multiply(mr.m)
 	// vfor j := range sC64 {
 	// v	sC64[j] = sC64[j] * mr.m
