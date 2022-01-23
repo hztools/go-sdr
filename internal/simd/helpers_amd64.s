03162523200c31c1df30611889123c04027eae49
Paul R. Tagliamonte <paultag@gmail.com>
2022-01-23 15:32:40 -0500

    [simd] update more build tag headers

--- internal/simd/helpers_amd64.s
+++ internal/simd/helpers_amd64.s
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE. }}}
 
+//go:build sdr.simdtest
 // +build sdr.simdtest
 
 #include "helpers_amd64.h"
--- internal/simd/helpers_arm64.s
+++ internal/simd/helpers_arm64.s
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE. }}}
 
+//go:build sdr.simdtest
 // +build sdr.simdtest
 
 #include "helpers_arm64.h"
