1919433e0725c473a8a8ffa066e08f070ec789c1
Paul R. Tagliamonte <paultag@gmail.com>
2022-06-14 22:22:39 -0400

    [stream] Add in a Beamform Reader
    This takes in a set of sdr.Readers which are assumed to be in
    phase-lock. Given the physical layout of the Antennas and the frequency
    of operation, and applies a rotation to the phase of each sample to
    beamform a single sdr.Reader out.

---
+++ stream/beamform.go
@@ -0,0 +1,75 @@
+// {{{ Copyright (c) Paul R. Tagliamonte <paul@k3xec.com>, 2022
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. }}}
+
+package stream
+
+import (
+	"fmt"
+
+	"hz.tools/sdr"
+)
+
+type Beamform struct {
+	sdr.Reader
+
+	readers sdr.Readers
+	config  BeamformConfig
+}
+
+func (b *Beamform) SetPhaseAngles(angles []complex64) error {
+	if len(angles) != len(b.readers) {
+		return fmt.Errorf("Beamform.SetPhaseAngles: angles must match the reader length")
+	}
+	for i, reader := range b.readers {
+		reader.(*multiplyReader).SetMultiplier(angles[i])
+	}
+	return nil
+}
+
+type BeamformConfig struct {
+	Angles []complex64
+}
+
+func ReadBeamform(rs sdr.Readers, cfg BeamformConfig) (*Beamform, error) {
+	multReaders := make(sdr.Readers, len(rs))
+	for i := range rs {
+		reader, err := ConvertReader(rs[i], sdr.SampleFormatC64)
+		if err != nil {
+			return nil, err
+		}
+		multReaders[i], err = Multiply(reader, 1)
+		if err != nil {
+			return nil, err
+		}
+	}
+	addReader, err := Add(multReaders...)
+	if err != nil {
+		return nil, err
+	}
+	b := &Beamform{
+		Reader:  addReader,
+		readers: multReaders,
+		config:  cfg,
+	}
+	b.SetPhaseAngles(cfg.Angles)
+	return b, nil
+}
+
+// vim: foldmethod=marker
