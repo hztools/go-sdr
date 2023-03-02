632f940ec8bfabc9935b3dfd275741f65d338ff8
Paul R. Tagliamonte <paultag@gmail.com>
2023-03-01 19:40:47 -0500

    [uhd] Fix C API types
    I was using type_t rather than int64_t - where I go that, I am not sure.
    I don't see this build failure on Debian, but it apparently happens on
    other platforms. This is my best whack at fixing it without thinking too
    hard about it. It still works on Debian and is at minimum more right.
    
    Build failure (Thanks, Steve):
    
    uhd/clock.go:38:41: cannot use _Ctype_time_t(secs) (value of type _Ctype_long) as _Ctype_longlong value in variable declaration
    uhd/clock.go:52:41: cannot use _Ctype_time_t(secs) (value of type _Ctype_long) as _Ctype_longlong value in variable declaration
    uhd/clock.go:85:3: cannot use &secs (value of type *_Ctype_long) as *_Ctype_longlong value in variable declaration
    uhd/rx.go:201:34: cannot use secs (variable of type _Ctype_long) as _Ctype_longlong value in assignment
    uhd/tx.go:251:71: cannot use secs (variable of type _Ctype_long) as _Ctype_longlong value in variable declaration

--- uhd/clock.go
+++ uhd/clock.go
@@ -36,7 +36,7 @@ func (s *Sdr) SetTimeNextPPS(d time.Duration) error {
 
 	return rvToError(C.uhd_usrp_set_time_next_pps(
 		*s.handle,
-		C.time_t(secs),
+		C.int64_t(secs),
 		C.double(frac),
 		0,
 	))
@@ -50,7 +50,7 @@ func (s *Sdr) SetTimeNow(d time.Duration) error {
 	// TODO(paultag): Multiple Mboards?
 	return rvToError(C.uhd_usrp_set_time_now(
 		*s.handle,
-		C.time_t(secs),
+		C.int64_t(secs),
 		C.double(frac),
 		0,
 	))
@@ -58,15 +58,15 @@ func (s *Sdr) SetTimeNow(d time.Duration) error {
 
 // splitDuration will split a time.Duration into USRP's time format, which
 // is whole seconds and fractional seconds.
-func splitDuration(d time.Duration) (C.time_t, C.double) {
+func splitDuration(d time.Duration) (C.int64_t, C.double) {
 	secs := d.Seconds()
 	frac := float64(d.Nanoseconds()) / 1e+9
-	return C.time_t(secs), C.double(frac)
+	return C.int64_t(secs), C.double(frac)
 }
 
 // newDuration will create a time.Duration from USRP's time format, made
 // up of whole seconds and fractional seconds.
-func newDuration(secs C.time_t, frac C.double) time.Duration {
+func newDuration(secs C.int64_t, frac C.double) time.Duration {
 	return time.Duration((float64(time.Second) * float64(frac)) +
 		(float64(time.Second) * float64(secs)))
 }
@@ -75,7 +75,7 @@ func newDuration(secs C.time_t, frac C.double) time.Duration {
 // requested.
 func (s *Sdr) GetTimeNow() (time.Duration, error) {
 	var (
-		secs C.time_t
+		secs C.int64_t
 		frac C.double
 	)
 
