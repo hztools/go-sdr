7db73c5e2a24524018dc58e3b034413bcb8fcef8
Paul R. Tagliamonte <paultag@gmail.com>
2023-06-22 13:58:33 -0400

    [adrv9364] Add in the device names I think I'll be using.
    I need to actually write the code here and check this works like the
    PlutoSDR does, and has a similar interface.

--- adrv9364/adrv9364.go
+++ adrv9364/adrv9364.go
@@ -28,4 +28,18 @@ func init() {
 	debug.RegisterRadioDriver("hz.tools/sdr/adrv9364.Sdr")
 }
 
+var (
+	// adrv9364PhyName is the name of the transceiver itself, used for control
+	// over things like samples per second or frequency.
+	adrv9364PhyName = "ad9361-phy"
+
+	// adrv9364RxName is the name of the RX ADC, to read samples from the rx
+	// antenna.
+	adrv9364RxName = "cf-ad9361-lpc"
+
+	// adrv9364TxName is the name of the TX DAC, to write samples to the
+	// tx side of the house.
+	adrv9364TxName = "cf-ad9361-dds-core-lpc"
+)
+
 // vim: foldmethod=marker
